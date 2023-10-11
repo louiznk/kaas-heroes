5c03b0228e0fbbbd5b2719f3858899a68ad29dfa
Louis Tournayre <louis.tournayre@zenika.com>
2023-10-11 17:16:02 +0200

    Maj dockers images pour upgrade clusterctl & fix kind load

--- demos/capi/offline/import_k3d.sh
+++ demos/capi/offline/import_k3d.sh
@@ -5,10 +5,13 @@ pushd $DIR
 clustername=$1
 while read -r line
 do 
-  short="${line##*/}.tar"
-  echo "Load $line docker image in k3d $clustername cluster"
-  echo "k3d image import -c $clustername 'images/$short'"
-  k3d image import -c $clustername "images/$short"
+  if ! [[ "$line" =~ ^#.* ]]
+  then
+    short="${line##*/}.tar"
+    echo "Load $line docker image in k3d $clustername cluster"
+    echo "k3d image import -c $clustername 'images/$short'"
+    k3d image import -c $clustername "images/$short"
+  fi
 done < images.txt
 
 popd
