d51fd363adfef68f8d7bbb36cb52d183e54a32b2
Louis Tournayre <louis.tournayre@zenika.com>
2023-10-12 13:19:35 +0200

    Simplifications

--- demos/capi/03-install_cni_and_ingress.sh
+++ demos/capi/03-install_cni_and_ingress.sh
@@ -12,13 +12,13 @@ DIR=$(dirname "$0")
 
 pushd $DIR
 
-# prompt "Install CALICO CNI"
-# pe 'kubectl --kubeconfig=$CLUSTER_KUBECONFIG apply -f ./calico'
-# ps1
+prompt "Install CALICO CNI"
+pe 'kubectl --kubeconfig=$CLUSTER_KUBECONFIG apply -f ./calico'
+ps1
 
-# prompt "Install traefik"
-# pe 'kubectl --kubeconfig=$CLUSTER_KUBECONFIG apply -f ./traefik'
-# ps1
+prompt "Install traefik"
+pe 'kubectl --kubeconfig=$CLUSTER_KUBECONFIG apply -f ./traefik'
+ps1
 
 set +e
 HTTPS_IP=$(gcloud compute addresses list --filter="name=$CLUSTER_NAME-https-lb-static-ipv4" --format="csv(address)" 2>/dev/null | tail -n 1)
